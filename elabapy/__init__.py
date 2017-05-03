b3c31dc3445fdee9f86bc027c25ce0b68556cdf0
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-05-03 11:17:17 +0200

    0.3.0

--- elabapy/__init__.py
+++ elabapy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """A python module to interact with elabftw API"""
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 __author__ = "Nicolas CARPi"
 __author_email__ = "nicolas.carpi@curie.fr"
 __license__ = "GPL v3"
