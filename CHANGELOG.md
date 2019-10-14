2af6c1a82fca85e3e3bb42b612da431fa3c5db60
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-10-15 01:07:14 +0200

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,36 @@
+# Changelog for elabapy
+# https://github.com/elabftw/elabapy
+# https://www.elabftw.net
+
+# Version 0.5.0
+
+## Added
+
+* Create item
+* Get items types
+* Get status
+* Add tags
+* Add link
+
+# Version 0.4.0
+
+## Added
+
+* Create experiment
+
+# Version 0.3.0
+
+## Added
+
+* File upload (fix #2)
+* Update instructions in README
+
+# Version 0.2.0
+
+## Added
+
+* Function to POST data (fix elabftw/elabftw#378)
+
+## Changed
+
+* Remove the documentation from the README as it's on doc.elabftw.net
--- example-script.py
+++ example-script.py
@@ -31,27 +31,27 @@ def someFunctionThatGetsDataFromSomething():
     """ This function fetches metadata from some source """
     return "Updated from API", "This is my data"
 
-# first create the "manager" with the token and endpoint URL
-manager = elabapy.Manager(token=API_KEY, endpoint=ENDPOINT)
+# first create the "elab" object with the token and endpoint URL
+elab = elabapy.manager(token=API_KEY, endpoint=ENDPOINT)
 
 # create an experiment
-new = manager.create_experiment()
+new = elab.create_experiment()
 exp_id = new["id"]
 print(f"New experiment created with id: {exp_id}")
 
 # the newly created experiment is empty
-my_exp = manager.get_experiment(exp_id)
+my_exp = elab.get_experiment(exp_id)
 print(json.dumps(my_exp, indent=4, sort_keys=True))
 
 # let's modify it
 title, body = someFunctionThatGetsDataFromSomething()
 params = {"title": title, "date": today_date, "body": body}
-out = manager.post_experiment(exp_id, params)
+out = elab.post_experiment(exp_id, params)
 print(f"Updating experiment with id {exp_id}: {out['result']}")
 
 # now let's create an item
 # first we need to know what are the available ids to us for the item type (as these are dynamic)
-items_types = manager.get_items_types()
+items_types = elab.get_items_types()
 print(json.dumps(items_types, indent=4))
 print(items_types[0]["category_id"])
 
@@ -59,21 +59,21 @@ print(items_types[0]["category_id"])
 # the parameter here is the id of an item_type!
 # for this script I'm simply using the first result of the previous query
 # but you'll want to use the same "category_id" probably
-my_item = manager.create_item(items_types[0]["category_id"])
+my_item = elab.create_item(items_types[0]["category_id"])
 item_id = my_item["id"]
 print(f"New item created with id: {item_id}")
 
 # now update it
-out = manager.post_item(item_id, params)
+out = elab.post_item(item_id, params)
 
 # attach a file to it
 my_file = {"file": open("example-script.py", "rb")}
-manager.upload_to_item(item_id, my_file)
+elab.upload_to_item(item_id, my_file)
 
 # now link our experiment to that database item
 params = {"link": item_id}
-manager.add_link_to_experiment(exp_id, params)
+elab.add_link_to_experiment(exp_id, params)
 
 # and add a tag to our experiment
 params = {"tag": "automated entry"}
-manager.add_tag_to_experiment(exp_id, params)
+elab.add_tag_to_experiment(exp_id, params)
